e2a67387cefe92dd9dec24251537ea4d46ff9f89
ryanschaefer <contact.devsquid@gmail.com>
2020-12-01 19:19:14 -0600

    Error handling

--- src/wrapper.rkt
+++ src/wrapper.rkt
@@ -1,8 +1,10 @@
 #!/usr/bin/env racket
 #lang racket
+(require racket/exn)
 (require aws)
 (require net/head)
 (require net/http-client)
+(require json)
 
 (define module (make-parameter ""))
 (define function (make-parameter ""))
@@ -33,17 +35,27 @@
 (define timeout-epoch (extract-field "Lambda-Runtime-Deadline-Ms" (event)))
 
 (define (wrap-handler)
+  (define host-port (string-split (getenv "AWS_LAMBDA_RUNTIME_API") ":"))
   (with-handlers ([exn:fail? (lambda (exn)
-                               (displayln (exn-message exn))
+                               (define-values (type skipped?) (struct-info exn))
+                               (http-sendrecv
+                                (first host-port)
+                                (string-append "/2018-06-01/runtime/invocation/"
+                                               request-id
+                                               "/error")
+                                #:method 'POST
+                                #:data  (jsexpr->string
+                                         (hasheq 'errorType (format "~a" type)
+                                                 'errorMessage (exn->string exn)))
+                                #:port (string->number (second host-port)))
                                #f)])
     (define response ((dynamic-require (module) (function)) (event)))
-    (define host-port (string-split (getenv "AWS_LAMBDA_RUNTIME_API") ":"))
     (writeln response)
     (http-sendrecv
      (first host-port)
      (string-append "/2018-06-01/runtime/invocation/" request-id "/response")
      #:method 'POST
-     #:data "SUCCESS"
+     #:data response
      #:port (string->number (second host-port)))))
 
 (define (main)
